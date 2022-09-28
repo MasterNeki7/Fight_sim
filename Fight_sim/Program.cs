f9bbfb50fd1e5876cfff2b8dc6f0405d146b0d94
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-09-28 14:46:59 +0200

    test

--- Fight_sim/Program.cs
+++ Fight_sim/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name;
+
+string name2;
+
+Console.WriteLine("welcome fighter. What is your name (you can't say cum)");
+name = Console.ReadLine();
+
+if (name == "cum")
+{
+ Console.WriteLine("No! you can't have that");
+}
+
+else
+{
+    Console.WriteLine($"Welcome {name}");
+    Console.WriteLine("What is your opponents name? (You can't say cum)");
+    name2 = Console.ReadLine();
+    
+    if (name2 == "hitler")
+    {
+        Console.WriteLine("You are stupid and stop please");
+    }
+}
+
+Console.ReadLine();
