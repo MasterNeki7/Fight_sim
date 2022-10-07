082724069cae38e347ee4a19dd93731682795a4e
Neo Babic <neo.babic@elev.ga.ntig.se>
2022-10-07 12:15:08 +0200

    lol kod

--- Fight_sim/Program.cs
+++ Fight_sim/Program.cs
@@ -1,6 +1,4 @@
-﻿Random generator = new Random();
-
-int hitChance = generator.Next(100);
+﻿
 
 string nameOne;
 
@@ -23,13 +21,74 @@ nameTwo = Console.ReadLine();
 
 Console.WriteLine($"Alright. {nameOne} is fighting {nameTwo}. BEGIN!");
 
-while (p1Hp > 0 || p2Hp > 0)
+Console.ReadLine();
+
+
+
+
+
+
+
+
+while (p1Hp > 0 && p2Hp > 0)
 {
+
+    Console.Clear();
+    Console.WriteLine($"{nameOne} har {p1Hp} HP");
+    Console.WriteLine($"{nameTwo} har {p2Hp} HP");
+
+
+    Random generator = new Random();
+
+    int hitChance = generator.Next(100);
+
+    Console.ReadLine();
     
+    if (hitChance > 50)
+
+    {
+        p2Hp -= 1;
+        Console.WriteLine("Hit!");
+        
+    }
+    else
+    {
+        Console.WriteLine("Miss!");
+        
+    }
+
+    Console.ReadLine();
+
+    hitChance = generator.Next(100);
+
+    if (hitChance > 50)
+    {
+        p1Hp -= 1;
+        Console.WriteLine("Hit!");
+        
+
+    }
+    else
+    {
+        Console.WriteLine("Miss");
+       
+    }
+
+    
+    
+
 }
 
+if (p1Hp < 0)
+    {
+        Console.WriteLine($"{nameTwo} Won!");
 
+    }
 
+if (p2Hp < 0)
+    {
+        Console.WriteLine($"{nameOne} Won!");
+    }
 
 
 Console.ReadLine();
